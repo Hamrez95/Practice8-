84b164525b9121e8f8c5ed50d8824dde3e988fe6
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-08-23 14:59:35 +0330

    Add project files.

---
+++ Part1Anagram.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice8
+{
+    public class Part1Anagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            char[] sortedS = s.ToCharArray();
+            char[] sortedT = t.ToCharArray();
+
+            Array.Sort(sortedS);
+            Array.Sort(sortedT);
+
+            for (int i = 0; i < sortedS.Length; i++)
+            {
+                if (sortedS[i] != sortedT[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+    
+
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Part One : Is Anagram Or Not **************************************");
+            Part1Anagram part1 = new();
+            Console.WriteLine(part1.IsAnagram("anagram", "nagaram"));
+            Console.WriteLine(part1.IsAnagram("rat", "car"));        
+            Console.WriteLine("*********************************************************************");
+
+            //Part Two-------------------------------------------------------------------------------------------
+        }
+    }
+    
+}
