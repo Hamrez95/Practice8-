4a3d3dde893a271ee816b68893203cb0e4d81009
Hamrez95 <hamidrezaprogrammingr95@gmail.com>
2023-08-23 15:54:23 +0330

    Add Part2Digits

---
+++ Part2Digits.cs
@@ -0,0 +1,27 @@
+﻿namespace Practice8
+{
+    public class Part2Digits
+    {
+        public int AddDigits(int num)
+        {
+
+            while (num >= 10)
+            {
+                int digitSum = 0;
+
+                while (num > 0)
+                {
+                    digitSum += num % 10; //digitsum = digitsum+(num%10) Add the last digit of 'num' to 'digitSum'
+                    num /= 10;   //num = num/10 Remove the last digit from 'num'
+                }
+
+                num = digitSum;
+            }
+
+            return num;
+
+        }
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -1,17 +1,30 @@
 ﻿namespace Practice8
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("- Part One : Is Anagram Or Not **************************************");
             Part1Anagram part1 = new();
             Console.WriteLine(part1.IsAnagram("anagram", "nagaram"));
-            Console.WriteLine(part1.IsAnagram("rat", "car"));        
+            Console.WriteLine(part1.IsAnagram("rat", "car"));
+            Console.WriteLine(part1.IsAnagram("hamid", "dimah"));
             Console.WriteLine("*********************************************************************");
 
             //Part Two-------------------------------------------------------------------------------------------
+
+            Console.WriteLine("- Part Two : Add Digits ********************************************");
+            Part2Digits part2 = new();
+            Console.WriteLine($"For num = 38 , the answer is --> {part2.AddDigits(38)}");  
+            Console.WriteLine($"For num = 0 , the answer is --> {part2.AddDigits(0)}");  
+            Console.WriteLine($"For num = 248 , the answer is --> {part2.AddDigits(248)}");  
+            Console.WriteLine($"For num = 45678 , the answer is --> {part2.AddDigits(45678)}");
+            Console.WriteLine("*********************************************************************");
+
+            //Part Three-------------------------------------------------------------------------------------------
+
+
         }
     }
-    
+
 }
